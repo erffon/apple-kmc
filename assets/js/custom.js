d86b20b996dbaa0c5fbffb4598080e895cd6df90
erfan <erffon@gmail.com>
2024-12-24 20:37:39 +0330

    Feat: Add slick to page but not working yet

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".cat_slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+  });
+});
