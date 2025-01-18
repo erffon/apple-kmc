19a339f2b383d49da56a4f7a1a40568b0b23d14c
erffon <erffon@gmail.com>
2025-01-18 19:40:04 +0330

    Fix: product slider corrected

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
   $(".slider").slick({
     rtl: true,
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 3.5,
-    slideToScroll: 1,
+    slidesToScroll: 1,
     draggable: true,
     cssEase: "ease-out",
+  });
+});
+$(document).ready(function () {
+  $(".product_slider").slick({
+    rtl: true,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4.5, // Show 3 items (adjust as needed)
+    slidesToScroll: 1,
+    draggable: true,
+    cssEase: "ease-out",
+    arrows: true,
+    centerMode: false, // Disable center mode
+    initialSlide: 0, // Start from the first item
   });
 });
 
